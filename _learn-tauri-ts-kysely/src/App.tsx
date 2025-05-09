65b26110dcf74b45fbd61d89fa860220b4b27da2
PTPhong-PPL <PTPhongSWB@gmail.com>
2025-05-09 11:04:48 +0700

    adding ts version of project to test kysely with tauri sqlite

---
+++ _learn-tauri-ts-kysely/src/App.tsx
@@ -0,0 +1,14 @@
+import { createSignal } from "solid-js";
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <main class="w-full p-4">
+      
+    </main>
+  );
+}
+
+export default App;
